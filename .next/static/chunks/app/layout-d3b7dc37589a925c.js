395b28d06721c7715dac0d17b032ddf2c251e5fa
Abin Thankachan <abint16@gmail.com>
2025-06-06 16:02:53 +0530

    Initial

--- .next/static/chunks/app/layout-d3b7dc37589a925c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{8334:e=>{e.exports={style:{fontFamily:"'Noto Sans', 'Noto Sans Fallback'",fontStyle:"normal"},className:"__className_b97ccc"}},9323:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,9324,23)),Promise.resolve().then(a.t.bind(a,8334,23))},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[975,441,684,358],()=>s(9323)),_N_E=e.O()}]);
